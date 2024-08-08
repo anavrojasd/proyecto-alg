241294bd93f321378f47cc6376638314cadaf8e3
anavrojasd <rojas.ana@correo.unimet.edu.ve>
2024-08-08 14:00:11 -0400

    json

--- API.py
+++ API.py
@@ -1,8 +1,8 @@
 import requests as rq
 
 def cargar_API(link):
-    info=rq.get(link)
-    return info.json()
+    info=rq.get(link).json()
+    return info
 
 cargar_API("https://www.swapi.tech/api/")
 
