f52328c2e2107fe5357c5dadbf63a53c2767e697
argimiro08 <daniel.sanchez@correo.unimet.edu.ve>
2024-08-08 20:08:25 -0400

    cambios dani 1

--- API.py
+++ API.py
@@ -8,9 +8,9 @@ def cargar_API(link):
 
 def cargar_peliculas():
     lista_peliculas = []
-    peliculasAPI = cargar_API("https://www.swapi.tech/api/films")
+    peliculasAPI = cargar_API("https://www.swapi.tech/api/planets?page=1&limit=75")
     
-    for results in peliculasAPI['result']:
+    for results in peliculasAPI['results']:
         titulo = results['properties']['title']
         personajes = results['properties']['characters']
         numero_del_episodio= results['properties']['episode_id']
@@ -18,15 +18,32 @@ def cargar_peliculas():
         opening_crawl= results['properties']['opening_crawl']
         director= results['properties']['director']
         productores= results['properties']['producer']
-        url= results['properties']['url']
+        url_pelicula= results['properties']['url']
         planetas= results['properties']['planets']
         aeronaves= results['properties']['starships']
         vehiculos= results['properties']['vehicles']
         especies= results['properties']['species']
 
         pelicula = Pelicula(titulo, personajes,numero_del_episodio,fecha_de_lanzamiento,
-                            opening_crawl, director, productores, url, planetas, aeronaves, vehiculos, especies)
+                            opening_crawl, director, productores, url_pelicula, planetas, aeronaves, vehiculos, especies)
         lista_peliculas.append(pelicula)
+    return lista_peliculas
+
+def cargar_planetas():
+    lista_planetas= []
+    planetasAPI= cargar_API("https://www.swapi.tech/api/planets")
+
+    for results in planetasAPI["results"]:
+        url_planeta=cargar_API(results["url"])
+        nombre_planeta = url_planeta['result']['properties']['name']
+
+        #results["results"]["url"]
+    #return lista_planetas
+        print(nombre_planeta)
+cargar_planetas()
+
         
+
+
     #return lista_peliculas
 cargar_peliculas()
