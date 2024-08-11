7f3f94204f16f46ea84c177b36b2f05693c3b191
argimiro08 <daniel.sanchez@correo.unimet.edu.ve>
2024-08-10 21:47:56 -0400

    csv

--- Estadistica.py
+++ Estadistica.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as mat
+import matplotlib.pyplot as plt
 import numpy as num
 
 def csv_battles():
@@ -27,7 +27,6 @@ def csv_characters():
 
     # Display the dictionary
     print(diccionario_planeta)
-
 
 def csv_cities():
     # Specify the path to your Excel file
@@ -125,14 +124,67 @@ def csv_starships():
 
     # Read the Excel file
     df = pd.read_csv(file_path)
-
-    longitud_nave= dict(zip(df['name'], df['length']))
-    capacidad_nave= dict(zip(df['name'], df['cargo_capacity']))
-    hiperimpulso_nave= dict(zip(df['name'], df['hyperdrive_rating']))
-    mglt_nave= dict(zip(df['name'], df['MGLT']))
+    return df
     
-    for naves in longitud_nave:
-        
+def longitud_de_nave(df):
+    longitud_nave= dict(zip(df['name'], df['length']))
+    #Grafica para longitud
+    claves_longitud = list(longitud_nave.keys())
+    valor_longitud = list(longitud_nave.values())
+    fig, ax = plt.subplots()
+
+    # Display the pie chart
+    ax.pie(valor_longitud, startangle=140)
+    leyenda_titulos = [f'{label}: {size/ sum(valor_longitud) * 100:.1f}%' for label, size in zip(claves_longitud, valor_longitud)]
+
+    # Add the legend below the pie chart
+    ax.legend(leyenda_titulos, title="Naves", loc="upper center", bbox_to_anchor=(0.5,-0.05),ncol=1)
+    plt.show()
+   
+   
+   
+def capacidad_de_nave(df):
+    capacidad_nave= dict(zip(df['name'], df['cargo_capacity']))
+    #Grafica para capacidad
+    claves_capacidad = list(capacidad_nave.keys())
+    valor_capacidad= list(capacidad_nave.values())
+    fig, ax = plt.subplots()
+
+    # Display the pie chart
+    ax.pie(valor_capacidad, startangle=140)
+    leyenda_titulos = [f'{label}: {size/ sum(valor_capacidad) * 100:.1f}%' for label, size in zip(claves_capacidad, valor_capacidad)]
+
+    # Add the legend below the pie chart
+    ax.legend(leyenda_titulos, title="Naves", loc="upper center", bbox_to_anchor=(0.5,-0.05),ncol=1)
+    plt.show() 
+
+def hiperimpulso_de_nave(df):
+    hiperimpulso_nave= dict(zip(df['name'], df['hyperdrive_rating']))
+    claves_hiperimpulso = list(hiperimpulso_nave.keys())
+    valor_hiperimpulso= list(hiperimpulso_nave.values())
+    fig, ax = plt.subplots()
+
+    # Display the pie chart
+    ax.pie(valor_hiperimpulso, startangle=140)
+    leyenda_titulos = [f'{label}: {size/ sum(valor_hiperimpulso) * 100:.1f}%' for label, size in zip(claves_hiperimpulso, valor_hiperimpulso)]
+
+    # Add the legend below the pie chart
+    ax.legend(leyenda_titulos, title="Naves", loc="upper center", bbox_to_anchor=(0.5,-0.05),ncol=1)
+    plt.show() 
+
+def mglt_de_nave(df):
+    mglt_nave= dict(zip(df['name'], df['MGLT']))
+    claves_mglt = list(mglt_nave.keys())
+    valor_mglt= list(mglt_nave.values())
+    fig, ax = plt.subplots()
+
+    # Display the pie chart
+    ax.pie(valor_mglt, startangle=140)
+    leyenda_titulos = [f'{label}: {size/ sum(valor_mglt) * 100:.1f}%' for label, size in zip(claves_mglt, valor_mglt)]
+
+    # Add the legend below the pie chart
+    ax.legend(leyenda_titulos, title="Naves", loc="upper center", bbox_to_anchor=(0.5,-0.05),ncol=1)
+    plt.show() 
 
 
 
